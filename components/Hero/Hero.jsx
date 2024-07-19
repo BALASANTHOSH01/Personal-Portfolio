d9f5cbfffe20ae4123404bf05d7b5fa33144bc5d
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-20 03:06:14 +0530

    portfolio v1 updation2

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -9,19 +9,19 @@ import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <div id="home" className="section px-[2%] h-100vh">
+    <div id="home" className="section px-[2%] h-dvh mx-auto">
       <div className="ml-[5%] mt-[3%] flex flex-col items-center  justify-center py-[2%]">
         <div className=" w-[40%] md:w-[100%] text-center bg-[var(--secondaryColor)] px-[10px] text-gray-300 py-[5px] text-lg md:text-sm rounded-[50px] relative border my-[2%]">
           {/* <WingIcon className="text-jade-500 text-[40px] absolute right-[-30px] top-[-40px]" /> */}
           <p>Hello, I'm Bala Santhosh, a Software Engineer</p>
         </div>
 
-        <div className=" flex flex-col items-center justify-center">
+        <div className=" flex flex-col items-center md:mt-[2%] justify-center">
           <div className="text-[80px] md:text-[40px] text-white text-center">
             <div className=" flex md:flex-col md:items-center items-start gap-3 justify-center relative z-40">
               {/* <StarIcon className="absolute z-0 text-gray-800 text-[40px] right-0"/> */}
-              <p> I'm a</p>
-              <p className="text-jade-500">
+              <p>I'm a {" "}
+              <span className="text-jade-500">
                 <TypeAnimation
                   preRenderFirstString={true}
                   sequence={[
@@ -36,12 +36,13 @@ const Hero = () => {
                   speed={2}
                   repeat={Infinity}
                 />
-              </p>{" "}
+              </span>
+              </p>
             </div>
             <p>who codes for growth</p>
           </div>
 
-          <div className=" w-[50%] md:w-[100%] text-[20px] text-center text-gray-400">
+          <div className=" w-[50%] md:mt-[2%] md:w-[98%] text-[20px] text-center text-gray-400">
             <p>
             I'm a trailblazing software developer with a passion for innovation and proven real-world success. Ready to channel my skills and drive to elevate a dynamic company and achieve remarkable results.
             </p>
--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,11 +1,28 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import { Jost } from "next/font/google";
+
 const jost = Jost({ subsets: ["latin"] });
 
 const NavBar = () => {
   const [activeItem, setActiveItem] = useState("Home");
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Set the initial value
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleActiveItem = (item) => {
     setActiveItem(item);
@@ -18,9 +35,7 @@ const NavBar = () => {
         smooth={true}
         duration={500}
         offset={-70}
-        className={`w-[16%] rounded-[50px] px-[17px] md:px-[2%] cursor-pointer md:py-[5px] py-[10px] text-center overflow-hidden md:text-black text-white text-[17px] md:text-[17px] ${
-          activeItem === itemsName ? 'bg-jade-600 md:text-white' : ''
-        }`}
+        className={`w-[16%] rounded-[50px] px-[17px] md:px-[3%] cursor-pointer md:py-[5px] py-[10px] text-center overflow-hidden md:text-black text-white text-[17px] md:text-[20px] ${activeItem === itemsName ? 'bg-jade-600 md:text-white' : ''}`}
         onClick={() => handleActiveItem(itemsName)}
       >
         <p>{itemsName}</p>
@@ -29,17 +44,17 @@ const NavBar = () => {
   };
 
   return (
-    <div className="flex flex-row items-center md:fixed md:bottom-2 md:left-[5%] w-[90%] mx-auto px-[12px] md:px-[0px] bg-[var(--secondaryColor)] rounded-[50px] justify-between mt-[1%] border overflow-hidden md:bg-white">
+    <div className="flex flex-row items-center md:fixed md:bottom-5 md:left-[5%] w-[90%] mx-auto px-[12px] md:px-[0px] bg-[var(--secondaryColor)] rounded-[50px] justify-between mt-[1%] border overflow-hidden md:bg-white">
       <NavItem itemsName="Home" to="home" />
-      <NavItem itemsName="About" to="about" />
-      <NavItem itemsName="Skills" to="skills" />
-
+      {!isMobileScreen && <>
+        <NavItem itemsName="About" to="about" />
+        <NavItem itemsName="Skills" to="skills" />
+      </>}
       <div className={`w-auto rounded-[50px] px-[17px] py-[10px] text-jade-500 font-bold text-[40px] md:text-[20px] md:hidden ${jost.className}`}>
         <p>BALASANTHOSH</p>
       </div>
-
       <NavItem itemsName="Projects" to="projects" />
-      <NavItem itemsName="Contact" to="contact" />
+      {!isMobileScreen && <NavItem itemsName="Contact" to="contact" />}
       <NavItem itemsName="Resume" to="resume" />
     </div>
   );
