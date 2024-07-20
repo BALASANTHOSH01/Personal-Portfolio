0ef7ebbf2a903a1127082b119d33b99ad2a7721a
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-07-20 11:44:29 +0530

    Mobile nav

--- components/NavBar/MobileNav.jsx
+++ components/NavBar/MobileNav.jsx
@@ -8,21 +8,21 @@ const MobileNav = () => {
         <div className="fixed bottom-0 w-full bg-white shadow-lg z-50">
             <div className="flex flex-row items-center justify-around py-2">
                 <Link href="/" passHref legacyBehavior>
-                    <a className="flex flex-col items-center w-1/3 text-gray-600 hover:text-blue-500">
+                    <a className="flex flex-col items-center w-1/3 text-gray-600 hover:text-jade-500">
                         <HomeIcon className="text-2xl" />
                         <span className="text-xs">Home</span>
                     </a>
                 </Link>
 
                 <Link href="/#projects" passHref legacyBehavior>
-                    <a className="flex flex-col items-center w-1/3 text-gray-600 hover:text-blue-500">
+                    <a className="flex flex-col items-center w-1/3 text-gray-600 hover:text-jade-500">
                         <ProjectsIcon className="text-2xl" />
                         <span className="text-xs">Projects</span>
                     </a>
                 </Link>
 
                 <a
-                    className="flex flex-col items-center w-1/3 text-gray-600 hover:text-blue-500"
+                    className="flex flex-col items-center w-1/3 text-gray-600 hover:text-jade-500"
                     href="https://drive.google.com/file/d/14EDTqJ5ZTYlb66K9Crup_tGkka_xxw7F/view?usp=drive_link"
                     target="_blank"
                     rel="noopener noreferrer"
